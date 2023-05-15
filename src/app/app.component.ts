6a4e50409a5091bd9c6ada92d55d4ef37e6d40f4
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-05-15 15:12:39 -0500

    Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PhotosService} from "./services/photos.service";
+import {Photo} from "./models/Photos";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'material-angular';
+export class AppComponent implements  OnInit{
+
+  albunsId = [1,2,3];
+  constructor(private servicePhoto: PhotosService) {
+  }
+  ngOnInit(): void {
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import { PhotosComponent } from './components/photos/photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatSelectModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
